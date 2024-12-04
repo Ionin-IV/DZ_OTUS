ba70c63b75232cf08cfea4ed8f526397452301cd
Ionin-IV <iionin@mail.ru>
2024-12-04 15:30:10 +0300

    Create cons.py

---
+++ Kafka/cons.py
@@ -0,0 +1,29 @@
+import json
+from confluent_kafka import Consumer, KafkaException
+
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'consumer1',
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(conf)
+
+topic = 'lab-py-topic'
+consumer.subscribe([topic])
+
+try:
+    while True:
+        msg = consumer.poll(timeout=1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                print(f'Конец раздела {msg.topic()} [{msg.partition()}]')
+            elif msg.error():
+                raise KafkaException(msg.error())
+        else:
+            data = json.loads(msg.value().decode('utf-8'))
+            print(f'Получено сообщение: {data}')
+finally:
+    consumer.close()
